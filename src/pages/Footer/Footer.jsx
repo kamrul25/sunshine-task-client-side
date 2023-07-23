483a9a660c8b084772dee1a5280c7dd35c21d65a
kamrul25 <kamrul.coder01@gmail.com>
2023-07-23 18:21:46 +0600

    add footer

--- src/pages/Footer/Footer.jsx
+++ src/pages/Footer/Footer.jsx
@@ -1,11 +1,11 @@
-
-
 const Footer = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <footer className="footer footer-center p-4  text-primary-content">
+      <div>
+        <p>Copyright &copy; 2023 - All right reserved by Job Portal Ltd</p>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
