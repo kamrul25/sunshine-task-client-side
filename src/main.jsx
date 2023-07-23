0918fcd3eb7ead01d6c2335e65b0fddd544c7a55
kamrul25 <kamrul.coder01@gmail.com>
2023-07-23 13:21:18 +0600

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+   <h1 className='text-3xl text-white ml-4'>Hi I am Kamrul Hasan Jaman</h1>
+  </React.StrictMode>,
+)
