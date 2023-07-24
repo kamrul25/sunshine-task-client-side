372e62931fa3ff38bd7974f8064b506bcb9f5331
kamrul25 <kamrul.coder01@gmail.com>
2023-07-24 18:51:59 +0600

    add banner and profile section

--- src/pages/Home/Sections/Banner.jsx
+++ src/pages/Home/Sections/Banner.jsx
@@ -1,9 +1,12 @@
-
+import hero from "../../../assets/hero-man.png.webp"
 
 const Banner = () => {
     return (
-        <div>
-            
+        <div className="lg:flex justify-evenly items-center gap-3">
+            <div className="text-center lg:text-justify text-primary-content ">
+                <h1 className="text-2xl lg:text-5xl font-bold">Searching for a <br />Job? Find the<br /><span className="text-[#007AFF]">best job</span> that<br /> <span className="text-[#007AFF]">fit for you</span></h1>
+            </div>
+            <img src={hero} alt="" />
         </div>
     );
 };
